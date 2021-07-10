b9d2be33c9f8b5b66ab52af96b73345ae61b156e
Andrew Magdy <andrewmagdygad@gmail.com>
2021-07-10 12:27:45 +0200

    fix: add new task

--- src/App.js
+++ src/App.js
@@ -1,96 +1,89 @@
-import './App.css';
-import {useSelector, useDispatch} from 'react-redux';
-import {addNewTask} from './components/taskSlice';
-import {useEffect, useState} from 'react';
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { addNewTask } from "./components/taskSlice";
+import { useEffect, useState } from "react";
+import { selectAllTasks, fetchTasks } from "./components/taskSlice";
 
 function App() {
-  // const dispatch = useDispatch();
-  // const tasks = useSelector(selectAllTasks);
+    const dispatch = useDispatch();
+    const tasks = useSelector(selectAllTasks);
 
-  // const taskStatus = useSelector((state) => state.task.status);
-  // const error = useSelector((state) => state.task.error);
+    const taskStatus = useSelector((state) => state.task.status);
+    const error = useSelector((state) => state.task.error);
 
-  // useEffect(() => {
-  //   if(taskStatus === 'idle')dispatch(fetchTasks());
-  // },[taskStatus,dispatch])
+    useEffect(() => {
+        if (taskStatus === "idle") dispatch(fetchTasks());
+    }, [taskStatus, dispatch]);
 
-  // let content
+    // let content;
+    // if (taskStatus === "loading") {
+    //     content = <div className="loader">Loading...</div>;
+    // } else if (taskStatus === "succeeded") {
+    //     content = tasks.map((task) => <li key={task.id}>{task.content}</li>);
+    // } else if (taskStatus === "error") {
+    //     content = <div>{error}</div>;
+    // }
+    // const addNewTasks = useSelector(addNewTask);
 
-  // if (taskStatus === 'loading') {
-  //   content = <div className="loader">Loading...</div>
-  // } else if (taskStatus === 'succeeded') {
+    const [content, setContent] = useState("");
+    const [dueString, setDueString] = useState("tomorrow at 12:00");
+    const [dueLang, setdueLang] = useState("en");
+    const [priority, setPriority] = useState(1);
 
-  //   content = tasks.map((task) => (
-  //     <li key={task.id} >{task.content}</li>
-  //   ))
-  // } else if (taskStatus === 'error') {
-  //   content = <div>{error}</div>
-  // }
-      const dispatch = useDispatch();
-      // const addNewTasks = useSelector(addNewTask);
-
-      const [content, setContent] = useState('');
-      const [dueString, setDueString] = useState('tomorrow at 12:00');
-      const [dueLang, setdueLang] = useState('en');
-      const [priority, setPriority] = useState('1');
-
-      const onSavePostClicked = async (e) => {
+    const onSavePostClicked = (e) => {
         e.preventDefault();
         let data = {
-          "content": content,
-          "due_string": dueString,
-          "due_lang": "en",
-          "priority": priority,
-          "project_id": 2269074557
-        }
-        let test = await dispatch(addNewTask(data));
-        console.log('app event :' , test);
-      }
+            content: content,
+            due_string: dueString,
+            due_lang: "en",
+            priority: priority,
+            project_id: 2269074557,
+        };
+        let test = dispatch(addNewTask(data));
+        console.log("app event :", test);
+    };
 
-
-  return (
-    <div className="App">
-      {/* <h2>tasks</h2>
-      <ul>
-        {content}
-      </ul> */}
-      <form>
-        <label htmlFor="postTitle">Task :</label>
-        <input
-          type="text"
-          placeholder="enter your task"
-          value={content}
-          onChange={(prev) => {
-            prev = prev.target.value;
-            setContent(prev);
-          }}
-        />
-        <label htmlFor="dueDate">Due Date :</label>
-        <input
-          type="text"
-          placeholder="enter date"
-          value={dueString}
-          onChange={(prev) => {
-            prev = prev.target.value;
-            setDueString(prev);
-          }}
-        />
-        <label htmlFor="priority">priority :</label>
-        <input
-          type="text"
-          placeholder="enter priority"
-          value={priority}
-          onChange={(prev) => {
-            prev = prev.target.value;
-            setPriority(prev);
-          }}
-        />
-        <button type="button" onClick={onSavePostClicked} >
-          Save Post
-        </button>
-      </form>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h2>tasks</h2>
+            <ul>{content}</ul>
+            <form>
+                <label htmlFor="postTitle">Task :</label>
+                <input
+                    type="text"
+                    placeholder="enter your task"
+                    value={content}
+                    onChange={(prev) => {
+                        prev = prev.target.value;
+                        setContent(prev);
+                    }}
+                />
+                <label htmlFor="dueDate">Due Date :</label>
+                <input
+                    type="text"
+                    placeholder="enter date"
+                    value={dueString}
+                    onChange={(prev) => {
+                        prev = prev.target.value;
+                        setDueString(prev);
+                    }}
+                />
+                <label htmlFor="priority">priority :</label>
+                <input
+                    type="text"
+                    placeholder="enter priority"
+                    value={priority}
+                    onChange={(prev) => {
+                        prev = prev.target.value;
+                        setPriority(prev);
+                    }}
+                />
+                <button type="button" onClick={onSavePostClicked}>
+                    Save Post
+                </button>
+            </form>
+        </div>
+    );
 }
 
 export default App;
--- src/components/taskSlice.js
+++ src/components/taskSlice.js
@@ -3,45 +3,53 @@ import { fetchActiveTasks, addTask } from "../api/client";
 
 const initialState = {
     todos: [],
-    status: 'idle',
-    error:null
-}
+    status: "idle",
+    error: null,
+};
 
-export const fetchTasks = createAsyncThunk('tasks/fetchTasks', async () => {
+export const fetchTasks = createAsyncThunk("tasks/fetchTasks", async () => {
     const res = await fetchActiveTasks();
-    console.log('taskSlice fetchtasks: ',res[0], typeof(res));
+    console.log("taskSlice fetchtasks: ", res[0], typeof res);
     return res;
-})
+});
 
-export const addNewTask = createAsyncThunk('tasks/addNewTask', async (newTask) => {
-    const res = await addTask(newTask);
-    return res;
-})
+export const addNewTask = createAsyncThunk(
+    "tasks/addNewTask",
+    async (newTask) => {
+        const res = await addTask(newTask);
+        console.log("Hereee :", res);
+        return res;
+    }
+);
 
 export const taskSlice = createSlice({
     name: "task",
     initialState,
-    reducers: {
-        
-    },
-    extraReducers: {
-        [fetchTasks.pending]: (state, action) => {
-            state.status = 'loading';
-        },
-        [fetchTasks.fulfilled]: (state, action) => {
-            state.status = 'succeeded';
-            state.todos = state.todos.concat(action.payload);
-        },
-        [fetchTasks.rejected]: (state, action) => {
-            state.status = 'failed';
-            state.error = action.payload;
-        },
-        [addNewTask.fulfilled]: (state, action) => {
-            state.status = 'succeeded';
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(addNewTask.fulfilled, (state, action) => {
+            console.log("addNewTask.fulfilled", action.payload);
+            // Add user to the state array
             state.todos.push(action.payload);
-        }
-    }
-})
+        });
+        // [fetchTasks.pending]: (state, action) => {
+        //     state.status = "loading";
+        // },
+        // [fetchTasks.fulfilled]: (state, action) => {
+        //     state.status = "succeeded";
+        //     state.todos = state.todos.concat(action.payload);
+        // },
+        // [fetchTasks.rejected]: (state, action) => {
+        //     state.status = "failed";
+        //     state.error = action.payload;
+        // },
+        // [addNewTask.fulfilled]: (state, action) => {
+        //     console.log("addNewTask.fulfilled", action.payload);
+        //     state.status = "succeeded";
+        //     state.todos.push(action.payload);
+        // },
+    },
+});
 
 export default taskSlice.reducer;
 
